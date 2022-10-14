ad23634bf31eb131c640241ecdceaa103457822d
Excelsior <33706074+epicexcelsior@users.noreply.github.com>
2022-10-14 09:10:29 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -127,9 +127,10 @@ for (const channel of channels) {
 
 	const raw = await readFile(new URL(fileName, resourcesDir), { encoding: 'utf8' });
 	const r1 = raw.replace(linkEscapeRegex, linkEscapeReplacer).replace(/"/g, '\\"');
+	console.log(r1);
 	const r2 = Object.entries(replacePatterns).reduce((acc, [k, v]) => acc.replace(new RegExp(k, 'gm'), v), r1);
+	console.log(r2);
 	const parts = r2.split('\n\n');
-	console.log(parts);
 
 	let firstMessage: APIMessage | null = null;
 	for (let part of parts) {
