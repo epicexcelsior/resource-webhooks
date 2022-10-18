0c802cf72cd64a765fc2f37477c85b5b844f611f
epicexcelsior <epicexcelsior@gmail.com>
2022-10-18 09:51:47 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -135,7 +135,7 @@ for (const channel of channels) {
 		if (firstMessage) {
 			part = part.replace(
 				jumpRegex,
-				`https://discord.com/channels/${firstMessage.guild_id}/${firstMessage.channel_id}/${firstMessage.id}`,
+				`https://discord.com/channels/777991471907078174/${firstMessage.channel_id}/${firstMessage.id}`,
 			);
 		}
 		// A raw API response is returned here, not a Message object as the typings indicate
